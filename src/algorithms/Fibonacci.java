141614789b5a5e46388c62fda1403aea163456fa
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-05-15 16:51:03 -0700

    I finished Fibonacci and started GoofyNames.

--- src/algorithms/Fibonacci.java
+++ src/algorithms/Fibonacci.java
@@ -1,43 +1,18 @@
 package algorithms;
 
 public class Fibonacci {
-public static void main(String[] args) {
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-}
+	public static void main(String[] args) {
+		int f1 = 0;
+		int f2 = 1;
+		int temp = 0;
+		int f3 = 0;
+		for (int i = 0; i < 12; i++) {
+			System.out.println(f2); 
+			temp = f1;
+			f1 = f2;
+			f2 = temp; 
+			f2=f1+f2;
+		}
+
+	}
 }
--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -8,13 +8,12 @@ import javax.swing.JOptionPane;
 
 public class GoofyNames {
 	public static void main(String[] args) {
-		String goofyName = "";
+		
 
 		// 1. Ask the user to enter their name
 String name=JOptionPane.showInputDialog("What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
 
